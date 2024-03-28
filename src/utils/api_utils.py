e0f89665203c7c8f91a9f0868385f24ec383b792
Kahmeeah Obey <kahmobey@gmail.com>
2024-03-28 14:49:24 -0400

    feat(api): implement DeepL auth

--- src/utils/api_utils.py
+++ src/utils/api_utils.py
@@ -8,7 +8,9 @@ load_dotenv()
 logger = logging.getLogger(__name__)  
 logger.setLevel(logging.INFO)  
 
+# import MT packages
 from google.cloud import translate
+import deepl
 
 """
 ************ Google Translate API Helper Functions ************
@@ -17,11 +19,11 @@ This section contains helper functions for interacting with the Google Translate
 """
 
 PROJECT_ID = os.environ.get('PROJECT_ID')
-credential_path = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+CREDENTIAL_PATH = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 assert PROJECT_ID
 PARENT = f"projects/{PROJECT_ID}"
 
-def test_google_authentication():
+def test_google_auth():
     try:
         # Perform a simple translation task to trigger authentication
         client = translate.TranslationServiceClient()
@@ -36,4 +38,23 @@ def test_google_authentication():
     except Exception as e:  
         logger.error("Google Translate API authentication failed: %s", e)
 
-test_google_authentication()
+test_google_auth()
+
+"""
+************ DeepL API Helper Functions ************
+
+This section contains helper functions for interacting with the DeepL API.
+"""
+DEEPL_AUTH_KEY = os.environ['DEEPL_AUTH_KEY']
+assert DEEPL_AUTH_KEY
+translator = deepl.Translator(DEEPL_AUTH_KEY)
+
+def test_deepl_auth():
+    try:
+        result = translator.translate_text("Hello, world!", target_lang="FR")
+        logger.info("DeepL API authentication successful.")  # "Bonjour, le monde !"
+
+    except Exception as e:
+        logger.error("DeepL API authentication failed: %s", e)
+
+test_deepl_auth()
