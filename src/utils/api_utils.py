6abd55e06ce0a52104a4d69966181c88aaf8a43b
Kahmeeah Obey <kahmobey@gmail.com>
2024-03-28 17:50:39 -0400

    feat(api): implement openai auth

--- src/utils/api_utils.py
+++ src/utils/api_utils.py
@@ -1,6 +1,8 @@
 # Functions to interact with GPT-4, Google Translate, DeepL, and CUBBITT APIs
 # Handle input/output formatting for each service
 import logging
+import openai
+import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,15 +13,18 @@ logger.setLevel(logging.INFO)
 # import MT packages
 from google.cloud import translate
 import deepl
+from openai import OpenAI
+
 
 """
-************ Google Translate API Helper Functions ************
-
-This section contains helper functions for interacting with the Google Translate API.
+API Helper Functions
+This section contains helper functions for interacting with the Google Translate, DeepL, and OpenAi API.
 """
+
+# ================= GOOGLE TRANSLATE API =================
 
 PROJECT_ID = os.environ.get('PROJECT_ID')
-CREDENTIAL_PATH = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+CREDENTIAL_PATH = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 assert PROJECT_ID
 PARENT = f"projects/{PROJECT_ID}"
 
@@ -40,11 +45,8 @@ def test_google_auth():
 
 test_google_auth()
 
-"""
-************ DeepL API Helper Functions ************
+# ================= DEEPL API ================= 
 
-This section contains helper functions for interacting with the DeepL API.
-"""
 DEEPL_AUTH_KEY = os.environ['DEEPL_AUTH_KEY']
 assert DEEPL_AUTH_KEY
 translator = deepl.Translator(DEEPL_AUTH_KEY)
@@ -58,3 +60,39 @@ def test_deepl_auth():
         logger.error("DeepL API authentication failed: %s", e)
 
 test_deepl_auth()
+
+
+
+# ================= CUBBITT API ================= 
+
+# response = requests.get("https://lindat.mff.cuni.cz/services/translation/api/v2/")
+# if response:
+#     data = response.json()
+#     print("Success!")
+# else:
+#     raise Exception(f"Non-success status code: {response.status_code}")
+
+# print(data)
+
+# cubbitt_models={
+#     "English->French (CUBBITT)":"en-fr",
+#     "French->English (CUBBITT)":"fr-en",
+# }
+
+
+# ================= OPENAI API ================= 
+
+OPENAI_MODEL = os.environ.get('OPENAI_MODEL')
+client = OpenAI(
+    api_key = os.environ.get('OPENAI_KEY')
+)
+
+def test_openai_auth():
+    try:
+        client.models.list()
+        logger.info("OpenAI API Authentication Successful")
+    except openai.AuthenticationError as e: 
+        logger.error("OpenAI API Authentication Failed: %s", e)
+
+test_openai_auth()
+
